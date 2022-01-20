4b711075075cb38d594cba4ff15f04228460dad1
Matthew Wang <matwan@bergen.org>
2022-01-20 16:12:28 -0500

    Finished porting prototypes; a bit of dummy data injection

--- routes/events.js
+++ routes/events.js
@@ -3,11 +3,35 @@ var router = express.Router();
 
 /* GET events "home" page - a list of all events. */
 router.get('/', function(req, res, next) {
-  res.render('events', { title: 'Events', style: "tables.css"});
+  res.render('events', { title: 'Events', style: "tables"});
 });
 
-router.get('/1', function(req, res, next) {
-  res.render('event', { title: 'Event Details', styles: ["tables.css", "event.css"]});
+router.get('/create', function(req, res, next) {
+  res.render('eventform', {title: "Create Event", style: "newevent"})
+})
+
+router.get('/:event_id', function(req, res, next) {
+  let event_id = req.params.event_id
+  // GET FROM DATABASE: Select query where event_id = event_id from URL
+  //For now, lets pretend
+  let event_data = {event_id: event_id,
+                  event_name: "Opening Ceremony", 
+                  event_location: "Auditorium",
+                  event_date: "May 1 (Sat)",
+                  event_time: "10:30 AM",
+                  event_duration: "30m",
+                  event_type: "Main",
+                  event_interest: "100",
+                  event_description: "Be there!"}
+
+  res.render('event', { title: 'Event Details', 
+                      styles: ["tables", "event"], 
+                      event_id : event_id, 
+                      event_data: event_data});
 });
+
+router.get('/:event_id/modify', function(req, res, next) {
+  res.render('eventform', {title: "Modify Event", style: "newevent"})
+})
 
 module.exports = router;
